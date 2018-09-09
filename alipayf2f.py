3ff892602232e419474ddad6ee5a179fa6d91829
zyhibook <39795988+zyhibook@users.noreply.github.com>
2018-09-09 15:26:20 +0800

    Update alipayf2f.py

--- alipayf2f.py
+++ alipayf2f.py
@@ -2,7 +2,7 @@
 from alipay import AliPay
 import qrcode  # 导入模块   
 alipay = AliPay(
-    appid=2017051207221985,
+    appid=2088722464320424,
     app_notify_url=https91vps.club20170526freenom,
     app_private_key_path=app_private_key_path.txt,
     alipay_public_key_path=alipay_public_key_path.txt  # 支付宝的公钥，验证支付宝回传消息使用，不是你自己的公钥,
